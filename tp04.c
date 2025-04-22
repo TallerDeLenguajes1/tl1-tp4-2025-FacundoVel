c3f06cd1dc1ee9903661fdc119c169e21dface71
Facundo <facundo.vel99@gmail.com>
2025-04-21 23:25:43 -0300

    Se crea funcion para quitar tarea y cambiar el puntero del nodo

--- tp04.c
+++ tp04.c
@@ -13,7 +13,9 @@ typedef struct{
     Nodo *siguiente;
 }Nodo;
 
-
+Nodo *CrearNodo(int id, const char *descripcion, int duracion);
+Nodo *CargarTareasPendientes();
+void InsertarNodo(Nodo ** lista, Nodo *nuevo);
 
 int main (){
 
@@ -35,4 +37,51 @@ Nodo *CrearNodo(int id, const char *descripcion, int duracion) {
 void InsertarNodo(Nodo ** lista, Nodo *nuevo) {
     nuevo->siguiente = *lista;
     *lista = nuevo;
+}
+
+Nodo *CargarTareasPendientes() {
+    Nodo *lista = NULL;
+    int idAuto = 1000;
+    char descripcion[100];
+    char opcion;
+
+    do {
+        printf("Ingresar descripcion de la tarea: ");
+        fflush(stdin);
+        fgets(descripcion, sizeof(descripcion), stdin);
+
+        int duracion = 10 + rand() % 91;
+
+        Nodo *nuevo = CrearNodo(idAuto++, descripcion, duracion);
+        InsertarNodo(&lista, nuevo);
+
+        printf("Tarea cargada con duracion de: %d minutos", duracion);
+        printf("\nDesea cargar otra tarea? (s/n): ");
+        scanf(" %c", &opcion);
+    }
+    while (opcion == 's' || opcion == 'S');
+    return lista;
+}
+
+Nodo *QuitarTarea(Nodo ** lista, int id){
+    Nodo *actual = lista;
+    Nodo *anterior = NULL;
+
+    while(actual != NULL && actual->T.TareaID != id) {
+        anterior = actual;
+        actual = actual->siguiente;
+    }
+    
+    if (actual == NULL){
+        return NULL;
+    }
+    if (anterior == NULL)
+    {
+        *lista = actual->siguiente;
+    } else {
+        anterior->siguiente = actual->siguiente;
+    }
+
+    actual->siguiente = NULL;
+    return actual;
 }
