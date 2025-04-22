20bf90eb4b4c1957f4b4376f1466fdd05ac79fd6
Facundo <facundo.vel99@gmail.com>
2025-04-21 22:02:37 -0300

    Se crea la estructura para el Nodo y sus datos

---
+++ tp04.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    TareaID;
+    char *descripcion;
+    int duracion;
+}Tarea;
+
+typedef struct{
+    Tarea T;
+    Nodo *siguiente;
+}Nodo;
+
+int main (){
+
+    return 0;
+}
